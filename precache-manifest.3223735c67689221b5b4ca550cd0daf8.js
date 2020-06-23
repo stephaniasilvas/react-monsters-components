0337e7f4957ac1a43c9a37bccca7ab407b0f6e26
Stephania <you@example.com>
2020-06-23 20:57:09 +0100

    Updates

---
+++ precache-manifest.3223735c67689221b5b4ca550cd0daf8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "032458a692007dd33e4702e0ca6ae888",
+    "url": "/react-monsters-components/index.html"
+  },
+  {
+    "revision": "89dbcedf30ceae4a31cd",
+    "url": "/react-monsters-components/static/css/main.752a0eaf.chunk.css"
+  },
+  {
+    "revision": "0ac27ccecaca5553eea2",
+    "url": "/react-monsters-components/static/js/2.640f7b8d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monsters-components/static/js/2.640f7b8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89dbcedf30ceae4a31cd",
+    "url": "/react-monsters-components/static/js/main.4976edc6.chunk.js"
+  },
+  {
+    "revision": "4cbb3a651edc0786397d",
+    "url": "/react-monsters-components/static/js/runtime-main.d3fde978.js"
+  }
+]);
